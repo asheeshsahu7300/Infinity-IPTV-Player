f643b7866c61766bbea3d96d0b84a906427df64c
asheesh sahu <asheeshsahu7300@gmail.com>
2026-07-29 00:21:01 +0530

    add header in player

--- app/player.tsx
+++ app/player.tsx
@@ -646,6 +646,7 @@ export default function PlayerScreen() {
             headers: {
               "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
               "Connection": "keep-alive",
+              "Accept-Encoding": "identity",
             },
           }}
           shouldPlay={autoPlay && isPlaying}
